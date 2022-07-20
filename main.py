e2bb7d862b0f4e8365764c212350961a53c85c10
ugrasK <ugurkaraer1989@gmail.com>
2022-07-20 22:11:36 +0300

    how to: write to a file, append text to a file, create a file to write.

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 # read a file
-with open("my_file.txt") as file:
-    contents = file.read()
-    print(contents)
+# with open("my_file.txt") as file:
+#     contents = file.read()
+#     print(contents)
+
+# write to a file
+# with open("my_file.txt", mode="w") as file:
+#     file.write("New text.")
+
+# write to a file with appending text in it
+# with open("my_file.txt", mode="a") as file:
+#     file.write("\nNew Text.")
+
+# create a file to write in it
+# with open("new_file.txt", mode="w") as file_2:
+#     file_2.write("New text.")
+
 
 
